d7b619ae26b5a9ff91995ce7aa2a6c7e2a335531
Miguel Penha <miguel.penha1@gmail.com>
2023-03-19 22:59:10 -0300

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,11 @@
-import { Client, Intents } from 'discord.js'
+import { ClientOptions, Intents, Client } from 'discord.js'
 
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
-})
+const intents: ClientOptions['intents'] = [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES
+]
+
+const client = new Client({ intents })
 
 export default client
